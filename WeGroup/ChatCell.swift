6573fc06ab2ecbdca9daf3cd23b4dc684e666ebd
Darrell Shi <darrell_authentic@yahoo.com>
2016-02-05 12:24:59 -0500

    Settings added tableView

---
+++ WeGroup/ChatCell.swift
@@ -0,0 +1,22 @@
+//
+//  ChatCell.swift
+//  WeGroup
+//
+//  Created by Darrell Shi on 2/4/16.
+//  Copyright © 2016 WeGroup Inc. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ WeGroup/SettingsView.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsView.swift
+//  WeGroup
+//
+//  Created by Darrell Shi on 2/5/16.
+//  Copyright © 2016 WeGroup Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SettingsView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("SettingCell")!
+        
+        return cell
+    }
+}
