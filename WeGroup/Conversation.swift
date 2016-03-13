c4920196171683d33019c107013e4dceaacadf13
Darrell Shi <darrell_authentic@yahoo.com>
2016-03-13 00:10:34 -0500

    Changed profile view for group chats to random colors instead of user profile images, improved display of group chat names

--- WeGroup/Conversation.swift
+++ WeGroup/Conversation.swift
@@ -13,10 +13,18 @@ class Conversation {
     var messages: [Message]
     var toUsers: [PFUser]
     var updatedAt: NSDate
+    var isGroupChat: Bool
+    var profileColor: UIColor?
     
     init(toUsers: [PFUser]) {
         self.toUsers = toUsers
         self.messages = [Message]()
         self.updatedAt = NSDate()
+        if toUsers.count == 1 {
+            self.isGroupChat = false
+        } else {
+            self.isGroupChat = true
+            profileColor = UIColor(red: CGFloat(Float(arc4random_uniform(256))/255), green: CGFloat(Float(arc4random_uniform(256))/255), blue: CGFloat(Float(arc4random_uniform(256))/255), alpha: 0.5)
+        }
     }
 }
--- WeGroup/ConversationCell.swift
+++ WeGroup/ConversationCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ConversationCell: UITableViewCell {
     @IBOutlet weak var usernameLabel: UILabel!
@@ -18,22 +19,32 @@ class ConversationCell: UITableViewCell {
     var conversation: Conversation! {
         didSet {
             var username = ""
-            for user in conversation.toUsers {
-                username += "\(user.username!) "
+            for index in 0...conversation.toUsers.count-1 {
+                username += "\(conversation.toUsers[index].username!)"
+                if index != conversation.toUsers.count-1 {
+                    username += ", "
+                }
             }
             usernameLabel.text = username
+            
             if let preview = conversation?.messages.last?.text {
                 previewLabel.text = preview
             } else {
                 previewLabel.text = ""
             }
+            
             profileImageView.contentMode = UIViewContentMode.ScaleAspectFill
             profileImageView.clipsToBounds = true
             profileImageView.layer.cornerRadius = 25
-            if let profileData = conversation?.toUsers.first?["profile_image"] as? NSData {
-                let image = UIImage(data: profileData)
-                profileImageView.image = image
+            if conversation.isGroupChat {
+                profileImageView.backgroundColor = conversation.profileColor
+            } else {
+                if let profileData = conversation?.toUsers.first?["profile_image"] as? NSData {
+                    let image = UIImage(data: profileData)
+                    profileImageView.image = image
+                }
             }
+            
             if let time = conversation?.updatedAt{
                 let dateFormatter = NSDateFormatter()
                 dateFormatter.dateFormat = "hh:mm"
@@ -45,7 +56,6 @@ class ConversationCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
