4e40ddbb5546a1e84be423ab49c3b6682926f8b8
Darrell Shi <darrell_authentic@yahoo.com>
2016-03-13 00:31:34 -0500

    Added algorithm for searching conversations that a friend is in

--- WeGroup/ConversationsView.swift
+++ WeGroup/ConversationsView.swift
@@ -16,11 +16,15 @@ class ConversationsView: UIViewController {
     @IBOutlet var tableView: UITableView!
     var deleting = false
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    var filteredConversations: [Conversation]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
                 
         timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: "onTimer", userInfo: nil, repeats: true)
         
@@ -30,6 +34,7 @@ class ConversationsView: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        filteredConversations = Data.conversations
         tableView.reloadData()
     }
     
@@ -76,30 +81,68 @@ class ConversationsView: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "ToChat" {
+            searchBarCancelButtonClicked(searchBar)
             let indexPath = sender as! NSIndexPath
             let vc = segue.destinationViewController as! MessageView
-            vc.conversation = Data.conversations[indexPath.row]
+            vc.conversation = filteredConversations![indexPath.row]
         }
     }
 }
 
-extension ConversationsView: UITableViewDelegate, UITableViewDataSource {
+extension ConversationsView: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Data.conversations.count
+        if let conversations = filteredConversations {
+            return conversations.count
+        }
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ChatCell") as! ConversationCell
-        cell.conversation = Data.conversations[indexPath.row]
+        cell.conversation = filteredConversations![indexPath.row]
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if deleting {
             Data.conversations.removeAtIndex(indexPath.row)
+            filteredConversations?.removeAtIndex(indexPath.row)
             tableView.reloadData()
         } else {
             self.performSegueWithIdentifier("ToChat", sender: indexPath)
         }
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if let searchText = searchBar.text {
+            if searchText == "" {
+                filteredConversations = Data.conversations
+            } else {
+                filteredConversations = Data.conversations.filter({ (conversation) -> Bool in
+                    for user in conversation.toUsers {
+                        let username = user.username
+                        if username?.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil, locale: nil) == nil {
+                            continue
+                        } else {
+                            return true
+                        }
+                    }
+                    return false
+                })
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        filteredConversations = Data.conversations
+        tableView.reloadData()
     }
 }
