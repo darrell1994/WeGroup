2ecfdba8a54cd327c4bf4a334809c80e4c1c09da
Darrell Shi <darrell_authentic@yahoo.com>
2016-03-26 21:30:57 -0400

    Added scroll view to profile view

--- WeGroup/ProfileView.swift
+++ WeGroup/ProfileView.swift
@@ -13,17 +13,28 @@ class ProfileView: UIViewController {
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var regionLabel: UILabel!
     @IBOutlet weak var shortBioLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentViewWidthConstraint: NSLayoutConstraint!
     var contact: Contact!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         usernameLabel.text = contact.username
         if let profileData = contact.profileImageData {
             profileImageView.image = UIImage(data: profileData)
         }
         regionLabel.text = contact.region
         shortBioLabel.text = contact.shortBio
+    }
+    
+    override func viewWillLayoutSubviews() {
+        contentViewWidthConstraint.constant = UIScreen.mainScreen().bounds.size.width
+
+        super.viewWillLayoutSubviews()
+        
+        contentViewHeightConstraint.constant = scrollView.frame.size.height+50
     }
 
     override func didReceiveMemoryWarning() {
